accd9c00ad9c6b1c114b28412d80d077b5c36c3a
Rentiii612 <yorentianarafu@gmail.com>
2026-07-18 11:38:55 +0700

    implementasi router

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::get('/login', [AuthController::class, 'login'])
-    ->name('login');
+// Login
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'process'])->name('login.process');
 
-Route::post('/login', [AuthController::class, 'process'])
-    ->name('login.process');
+// Dashboard (hanya bisa diakses jika sudah login)
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
+
+});
