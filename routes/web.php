acb05d2d210cf9cb86b47cdaf8db14e2de0ca519
Rentiii612 <yorentianarafu@gmail.com>
2026-07-18 10:55:12 +0700

    routers login admin dan kasir

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+Route::get('/login', [AuthController::class, 'login'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'process'])
+    ->name('login.process');
