ab613382956c94fc302cc828353eb20303466188
Rentiii612 <yorentianarafu@gmail.com>
2026-07-18 11:37:03 +0700

    mengimplementasikan autentikasi controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,9 +13,35 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    // Memproses login (akan kita isi nanti)
+    // Proses login
     public function process(Request $request)
     {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
+        if (Auth::attempt($credentials)) {
+
+            $request->session()->regenerate();
+
+            return redirect('/dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau Password salah!',
+        ]);
+    }
+
+    // Logout
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
